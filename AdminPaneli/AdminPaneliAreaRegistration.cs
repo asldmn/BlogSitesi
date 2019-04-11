e4b26bb56e15ab051f9359cb7066655baeff6229
asldmn <asli_duman_466@hotmail.com>
2019-04-11 09:52:49 +0300

    Add files via upload

---
+++ AdminPaneli/AdminPaneliAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace BlogSitesi.Areas.AdminPaneli
+{
+    //ETicaret gibi çok kapsamlı olmayan projelerde Admin panelini ayrı bir katman olarak yapmaya gerek yok.Projeye Add-->Area dan yeni bir alan açarız. Bu alanda Admin panelini yaparız.
+    public class AdminPaneliAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "AdminPaneli";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "AdminPaneli_default",
+                "AdminPaneli/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ AdminPaneli/Controllers/EtiketController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogSitesi.Areas.AdminPaneli.Controllers
+{
+    public class EtiketController : Controller
+    {
+        BlogSitesiEntities DB = new BlogSitesiEntities();
+
+        // GET: AdminPaneli/Etiket
+        public ActionResult EtiketListesiIndex()
+        {
+            return View(DB.Etiketler.ToList());
+        }
+    }
+}
