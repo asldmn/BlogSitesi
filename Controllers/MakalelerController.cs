7f383a113f05c577a8548b1a399a43f4274015d6
asldmn <asli_duman_466@hotmail.com>
2019-04-11 09:42:28 +0300

    Add files via upload

---
+++ Controllers/MakalelerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogSitesi.Controllers
+{
+    public class MakalelerController : Controller
+    {
+        BlogSitesiEntities DB = new BlogSitesiEntities();
+        // GET: Makaleler
+        public ActionResult MakaleListesiIndex()
+        {
+            return View();
+        }
+
+        public ActionResult KategoriListesiIndex()
+        {
+            return View();
+        }
+
+        //Model ile kategorileri listelemeyi partial'la yapıyoruz.
+        public ActionResult KategoriListele()
+        {
+            // add view---> create as a partial view'i seçeriz.(hazır olarak listeleme ve model ve veritabanı da seçebiliriz. ama yapmadık. Bu nedenle modeli View ile gönderdik.)
+            return View(DB.Kategoriler.ToList());
+        }
+    }
+}
